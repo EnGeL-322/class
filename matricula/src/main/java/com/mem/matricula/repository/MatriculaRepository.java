f4561770158427a9bd0ef1c5a30b6c380fbae4ba
DAD-2025-2 <you@example.com>
2025-09-30 14:54:22 -0500

    qwe

--- matricula/src/main/java/com/mem/matricula/repository/MatriculaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mem.matricula.repository;
-
-import com.mem.matricula.models.Matricula;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MatriculaRepository extends JpaRepository<Matricula, Long> {
-}
--- ms-cursos/src/main/java/com/mem/mscursos/repository/CursoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mem.mscursos.repository;
-
-import com.mem.mscursos.models.Curso;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CursoRepository extends JpaRepository<Curso, Long> {
-}
